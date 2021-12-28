52cfed427bbe54bba41064eda8f047a4dc0acd24
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2021-12-28 18:05:56 +0300

    connected microservices to eureka

--- customer-service/src/main/java/com/tolgakmbl/customerservice/proxy/BlacklistServiceProxy.java
+++ customer-service/src/main/java/com/tolgakmbl/customerservice/proxy/BlacklistServiceProxy.java
@@ -3,7 +3,8 @@ package com.tolgakmbl.customerservice.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="blacklist-service", url="localhost:9000")
+//@FeignClient(name="blacklist-service", url="localhost:9000")
+@FeignClient(name="blacklist-service")
 public interface BlacklistServiceProxy {
 
 	@GetMapping("/blacklist")
